864c2f93300965d030d3a13e7b2a236b628afaec
Linda Ramos <ltramos7@gmail.com>
2019-11-18 21:29:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,8 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array2 = []
-  array2.push("wow")
+  # array2 = []
+  # array2.push("wow")
 end
 
 def remove_element_from_end_of_array(array)
