145e9336ee3d73987606b3fe174a6cdcd9bd7e1b
Linda Ramos <ltramos7@gmail.com>
2019-11-18 20:58:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,24 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  be_a=["elem1", "elem2", "elem3", "elem4"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array1 = []
+  array1 << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array2 = []
+  array2.push("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array1 = []
+  array1.pop("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
