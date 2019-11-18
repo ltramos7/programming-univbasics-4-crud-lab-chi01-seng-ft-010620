493ee88eb648aeac22878228ec1212bc0cb0f7dd
Linda Ramos <ltramos7@gmail.com>
2019-11-18 21:05:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ end
 def remove_element_from_end_of_array(array)
   array = []
   removed_element = array.pop()
-  return removed_element
+  return "removed_element"
 end
 
 def remove_element_from_start_of_array(array)
